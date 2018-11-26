79d6ff0af03a9c5833db176bfbd46f74ca253bbe
look4suman <look4suman@gmail.com>
2018-11-25 21:28:44 -0500

    first commit

---
+++ HelloWorldForJenkins/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldForJenkins
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessge());
+        }
+
+        public static string CreateMessge()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ UnitTestProject/HelloWorldTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HelloWorldForJenkins;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class HelloWorldTests
+    {
+        [TestMethod]
+        public void TestHelloWorld()
+        {
+            Assert.AreEqual("Hello World", Program.CreateMessge());
+        }
+    }
+}
